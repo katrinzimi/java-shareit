d8623cf600441e09339bd4c9cb6f97a250738395
Katrinzi <katrin.zimii@yandex.ru>
2024-08-20 02:20:55 +0900

    ТЗ 15

--- src/main/java/ru/practicum/shareit/item/dto/CommentCreateDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentCreateDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import ru.practicum.shareit.item.model.Item;
 
 import java.time.LocalDateTime;
 
@@ -13,7 +12,5 @@ public class CommentCreateDto {
     private final Long id;
     @NotBlank
     private final String text;
-    private final Item item;
-    private final String authorName;
     private final LocalDateTime created;
 }

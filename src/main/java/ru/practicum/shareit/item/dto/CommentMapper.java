59dd572e0c833a7a036558f9ee527f98abbf606b
Katrinzi <katrin.zimii@yandex.ru>
2024-08-24 01:26:56 +0900

    ТЗ 15 исправления по ревью

--- src/main/java/ru/practicum/shareit/item/dto/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentMapper.java
@@ -9,15 +9,12 @@ import java.time.LocalDateTime;
 
 public class CommentMapper {
     public static CommentDto toCommentDto(Comment comment) {
-        if (comment != null) {
             return new CommentDto(
                     comment.getId(),
                     comment.getText(),
                     comment.getAuthor().getName(),
                     comment.getCreated()
             );
-        }
-        return null;
     }
 
     public static Comment toComment(CommentCreateDto commentDto, Item item, User user) {
